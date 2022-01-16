5a6a8f87f5aa9117a67cd5a10d6bfcaad82eada9
Alicia Amador <alicia.amador.cabezali@gmail.com>
2022-01-16 13:02:24 +0100

    Modificacion funciones 1nivel y añado funciones Lambda

--- funciones1nivel.py
+++ funciones1nivel.py
@@ -4,6 +4,11 @@ def normal(x):
 def cuadrado(x):
     return x *x
 
+
+def cubo(x):
+    return x**3
+
+
 def sumaTodos(limitTo,f):
     resultado=0
     for i in range(limitTo+1):
@@ -12,6 +17,6 @@ def sumaTodos(limitTo,f):
     return resultado
 
 
-
-print(sumaTodos(100,normal))
-print(sumaTodos(3,cuadrado))
+if __name__=='__main__':
+    print(sumaTodos(100,normal))
+    print(sumaTodos(3,cuadrado))
